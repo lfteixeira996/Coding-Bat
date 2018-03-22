c45fdd0ff41f36a3d4a22f53d572a20ce1c8a483
Luis Teixeira <lfteixeira@criticalsoftware.com>
2018-03-22 14:20:33 +0000

    new resolutions added

--- List-2/centered_average.py
+++ List-2/centered_average.py
@@ -1,7 +1,12 @@
 '''
 
-
-Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+Return the "centered" average of an array of ints, 
+which we'll say is the mean average of the values, 
+except ignoring the largest and smallest values in the array. 
+If there are multiple copies of the smallest value, ignore just
+one copy, and likewise for the largest value. Use int division 
+to produce the final average. You may assume that the array is 
+length 3 or more.
 
 
 centered_average([1, 2, 3, 4, 100]) → 3
@@ -9,6 +14,63 @@ centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
 centered_average([-10, -4, -2, -4, -2, 0]) → -3
 
 '''
+#Return max value
+def max_val(nums):
 
+    ret = nums[0]
+    
+    for i in range(1, len(nums)):
+        if ret < nums[i]:
+            ret = nums[i]
+            
+    
+    return ret
+
+#Return min value
+def min_val(nums):
+
+    ret = nums[0]
+    
+    for i in range(1, len(nums)):
+        if ret > nums[i]:
+            ret = nums[i]
+            
+    
+    return ret
+
+
+#Centered Average
 def centered_average(nums):
   
+   find_max = False
+   find_min = False
+   maxval   = max_val(nums)
+   minval   = min_val(nums)
+   num      = 0
+   den = 0
+   
+   for item in nums:
+       
+       if item != maxval and item != minval:
+           num += item
+           den += 1
+       
+       if item == maxval and find_max == True:
+           num += item
+           den += 1
+
+       if item == minval and find_min == True:
+           num += item
+           den += 1           
+        
+        
+       if item == maxval and find_max == False:
+           find_max = True
+           
+       if item == minval and find_min == False:
+           find_min = True
+               
+      
+   return num//den     
+   
+           
--- Logic-1/near_ten.py
+++ Logic-1/near_ten.py
@@ -1,6 +1,8 @@
 '''
 
-Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+Given a non-negative number "num", return True if num is within 2 
+of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, 
+so (7 % 5) is 2. See also: Introduction to Mod
 
 
 near_ten(12) → True
