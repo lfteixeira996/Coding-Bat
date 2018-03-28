89f145aded7c470e95aceef747511b916dff5c41
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-03-28 17:35:08 +0100

    Add files via upload

---
+++ Java/Warmup-2/stringTimes.java
@@ -0,0 +1,21 @@
+/**
+ * @author Luis Teixeira
+ *
+ * @date   28/03/2018
+ *
+ * @mail   filipe.teixeira.996@gmail.com
+ */
+package Warmup_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class stringTimes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
