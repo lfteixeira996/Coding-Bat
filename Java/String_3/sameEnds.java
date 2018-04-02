2fc3ffef6fafb47384dfb32a28f13a4ade03ae72
Luis Teixeira <filipe.teixeira.996@gmail.com>
2018-04-02 17:42:18 +0100

    Add files via upload

---
+++ Java/String_3/sameEnds.java
@@ -0,0 +1,60 @@
+/**
+ * @author Luis Teixeira
+ *
+ * @date   02/04/2018
+ *
+ * @mail   filipe.teixeira.996@gmail.com
+ */
+package String_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+
+Given a string, return the longest substring that appears at
+both the beginning and end of the string without overlapping. 
+For example, sameEnds("abXab") is "ab".
+
+
+sameEnds("abXYab") → "ab"
+sameEnds("xx") → "x"
+sameEnds("xxx") → "x" 
+ */
+public class sameEnds {
+
+	private static int i = 0;
+	
+	@Test
+	public void Test1()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sameEnds(\"abXYab\") → \"ab\"\n");
+		assertEquals("sameEnds(\"abXYab\") → \"ab\"","ab", sameEnds("abXYab"));
+		i++;
+	}
+	
+	@Test
+	public void Test2()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sameEnds(\"xx\") → \"x\"\n");
+		assertEquals("sameEnds(\"xx\") → \"x\"","x", sameEnds("xx"));
+		i++;
+	}
+	
+	@Test
+	public void Test3()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sameEnds(\"xxx\") → \"x\" \n");
+		assertEquals("sameEnds(\"xxx\") → \"x\"","x", sameEnds("xxx"));
+		i++;
+	}
+	
+	public String sameEnds(String string) {
+		  
+	}
+
+}
---
+++ Java/String_3/sumDigits.java
@@ -0,0 +1,71 @@
+/**
+ * @author Luis Teixeira
+ *
+ * @date   02/04/2018
+ *
+ * @mail   filipe.teixeira.996@gmail.com
+ */
+package String_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+
+Given a string, return the sum of the digits 0-9 that appear in the string, 
+ignoring all other characters. Return 0 if there are no digits in the string. 
+(Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+
+
+sumDigits("aa1bc2d3") → 6
+sumDigits("aa11b33") → 8
+sumDigits("Chocolate") → 0
+ */
+public class sumDigits {
+
+	private static int i = 0;
+	
+	@Test
+	public void Test1()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sumDigits(\"aa1bc2d3\") → 6\n");
+		assertEquals("sumDigits(\"aa1bc2d3\") → 6", 6, sumDigits("aa1bc2d3"));
+		i++;
+	}
+
+	@Test
+	public void Test2()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sumDigits(\"aa11b33\") → 8\n");
+		assertEquals("sumDigits(\"aa11b33\") → 8", 8, sumDigits("aa11b33"));
+		i++;
+	}
+	
+	@Test
+	public void Test3()
+	{
+		System.out.println("**********Test "+i+"**********");
+		System.out.println("sumDigits(\"Chocolate\") → 0\n");
+		assertEquals("sumDigits(\"Chocolate\") → 0", 0, sumDigits("Chocolate"));
+		i++;
+	}
+	
+	
+	
+	public int sumDigits(String str) {
+		
+		int sum = 0;
+		
+		for (int i = 0; i < str.length(); i++) {			
+			if (Character.isDigit(str.charAt(i))) {
+				sum += Character.getNumericValue(str.charAt(i));
+			}
+		}
+		
+		return sum;
+	}
+
+}
